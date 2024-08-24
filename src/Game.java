e65ded7e2834e771a19a8c844e04d60af459dc12
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-24 21:21:36 +0300

    add a class called UpAction

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Game {
+
+    Game(){
+
+    }
+
+    public  class  UpAction extends AbstractAction{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
 }
